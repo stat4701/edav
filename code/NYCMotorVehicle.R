26966425ad8fa6f2f181109184fd4ba73eef4435
kedarp <kedar.rp@gmail.com>
2015-02-23 23:24:00 -0500

    Map code added for >=15 per injuries

--- code/NYCMotorVehicle.R
+++ code/NYCMotorVehicle.R
@@ -145,31 +145,129 @@ require(scales) # for removing scientific notation
 
 
 
-#PLOT CONTRIBUTING FACTOR
-cont_factr_typ <- ddply(filter(nycmvc_data, DATE>"2012-12-31" & DATE<"2015-01-01"),
+#PLOT CONTRIBUTING FACTOR AND INJURY SCATTER PLOT
+filter_inj_veh_typ <- filter(nycmvc_data, DATE >"2012-12-31" & DATE <"2015-01-01")
+inj_veh_typ <- ddply(filter(nycmvc_data, DATE>"2012-12-31" & DATE<"2015-01-01" 
+                            & CONTRIBUTING.FACTOR.VEHICLE.1 != "" & CONTRIBUTING.FACTOR.VEHICLE.1 != "Unspecified"),
                      c("CONTRIBUTING.FACTOR.VEHICLE.1"), 
                      summarise,                 
-                     Inj_Pers=sum(NUMBER.OF.PERSONS.INJURED))
+                     Pedestrians=sum(NUMBER.OF.PEDESTRIANS.INJURED),
+                     Cyclists=sum(NUMBER.OF.CYCLIST.INJURED),
+                     Motorists=sum(NUMBER.OF.MOTORIST.INJURED))
+
+(bp1 <- ggplot(inj_veh_typ, aes(x=Pedestrians, y=Cyclists)) + geom_point(size=2) 
+            + ggtitle("Scatter plot Pedestrians vs Cyclists"))
+(bp2 <- bp1 + theme(plot.title=element_text(size=rel(2), face="bold")) 
+            + theme(axis.title=element_text(size=25))
+            + geom_text(aes(x=Pedestrians+500, y=Cyclists+50, label=CONTRIBUTING.FACTOR.VEHICLE.1), size=4, hjust=1))
+
+(bp1 <- ggplot(inj_veh_typ, aes(x=Pedestrians, y=Motorists)) + geom_point(size=2) 
+ + ggtitle("Scatter plot Pedestrians vs Cyclists"))
+(bp2 <- bp1 + theme(plot.title=element_text(size=rel(2), face="bold")) 
+ + theme(axis.title=element_text(size=25))
+ + geom_text(aes(x=Pedestrians+500, y=Motorists+500, label=CONTRIBUTING.FACTOR.VEHICLE.1), size=4, hjust=1))
+
+(bp1 <- ggplot(inj_veh_typ, aes(x=Motorists, y=Cyclists)) + geom_point(size=2) 
+ + ggtitle("Scatter plot Pedestrians vs Cyclists"))
+(bp2 <- bp1 + theme(plot.title=element_text(size=rel(2), face="bold")) 
+ + theme(axis.title=element_text(size=25))
+ + geom_text(aes(x=Motorists+500, y=Cyclists+50, label=CONTRIBUTING.FACTOR.VEHICLE.1), size=4, hjust=1))
 
 
-require(scales) # for removing scientific notation
+(bp1 <- ggplot(inj_veh_typ, aes(x=Pedestrians, y=Cyclists, z=Motorists)) + geom_point(size=2) 
+ + ggtitle("Scatter plot Pedestrians vs Cyclists"))
+(bp2 <- bp1 + theme(plot.title=element_text(size=rel(2), face="bold")) 
+ + theme(axis.title=element_text(size=25)))
+ + geom_text(aes(x=Pedestrians+500, y=Cyclists+50, label=CONTRIBUTING.FACTOR.VEHICLE.1), size=4, hjust=1))
 
-(ggplot(cont_factr_typ, aes(x=reorder(CONTRIBUTING.FACTOR.VEHICLE.1,Inj_Pers), y=Inj_Pers, fill=CONTRIBUTING.FACTOR.VEHICLE.1)) 
- + geom_bar(stat="identity")
- + coord_flip()
- #+ geom_text(aes(label=comma(Frequency)), vjust=-1, size=6, position=position_dodge(.9))
- + scale_y_continuous(labels = comma)
- + ggtitle("Contributing Factors for Injuries - 2013-2014")
+
+# Spinning 3D scatterplot
+# Install X11/XQuartz for Mac
+# Install and load rgl package
+install.packages("rgl")
+require("rgl")
+require("RColorBrewer")
+plot3d(inj_veh_typ$Pedestrians,  # x variable
+       inj_veh_typ$Cyclists,  # y variable
+       inj_veh_typ$Motorists,   # z variable
+       xlab = "Pedestrians",
+       ylab = "Cyclists",
+       zlab = "Motorists",
+       col  = "orange", 
+       size = 5)
+
+killedped=subset(nycmvc_data,NUMBER.OF.PEDESTRIANS.KILLED > 0)
+killedcyc=subset(nycmvc_data,NUMBER.OF.CYCLIST.KILLED > 0)
+killedmot=subset(nycmvc_data,NUMBER.OF.MOTORIST.KILLED > 0)
+
+library(ggmap)
+nycmap <- get_map("New York City", zoom="auto")
+
+(nyc1 <- ggmap(nycmap) + scale_color_discrete(name = "Fatal Accidents") +
+  geom_point(data=killedped, aes(LONGITUDE, LATITUDE, color = 'Pedestrians'), size=3) +
+  geom_point(data=killedcyc, aes(LONGITUDE, LATITUDE, color = 'Cyclists'), size=3) +
+  geom_point(data=killedmot, aes(LONGITUDE, LATITUDE, color = 'Motorists'), size=3))
+
+#CREATING SUMMARY TABLE
+library(plyr)
+#injureped=subset(nycmvc_data,(PEDS_HURT=(NUMBER.OF.PEDESTRIANS.INJURED+NUMBER.OF.PEDESTRIANS.KILLED)) > 0)
+injureped=subset(nycmvc_data,NUMBER.OF.PEDESTRIANS.INJURED > 0)
+injurecyc=subset(nycmvc_data,NUMBER.OF.CYCLIST.INJURED > 0)
+injuremtr=subset(nycmvc_data,NUMBER.OF.MOTORIST.INJURED > 0)
+
+injureped_loc <- ddply(injureped, c("LONGITUDE", "LATITUDE"), 
+                     summarise, Inj_No=sum(NUMBER.OF.PERSONS.INJURED))
+injurecyc_loc <- ddply(injurecyc, c("LONGITUDE", "LATITUDE"), 
+                       summarise, Inj_No=sum(NUMBER.OF.CYCLIST.INJURED))
+injuremtr_loc <- ddply(injuremtr, c("LONGITUDE", "LATITUDE"), 
+                       summarise, Inj_No=sum(NUMBER.OF.MOTORIST.INJURED))
+
+injureped_loc_sum <- ddply(injureped_loc, c("Inj_No"), 
+                       summarise, FreqCnt=length(Inj_No))
+injurecyc_loc_sum <- ddply(injurecyc_loc, c("Inj_No"), 
+                           summarise, FreqCnt=length(Inj_No))
+injuremtr_loc_sum <- ddply(injuremtr_loc, c("Inj_No"), 
+                           summarise, FreqCnt=length(Inj_No))
+
+high_injure_loc_ped <- subset(injureped_loc, Inj_No >= 15)
+high_injure_loc_cyc <- subset(injurecyc_loc, Inj_No >= 10)
+high_injure_loc_mtr <- subset(injuremtr_loc, Inj_No >= 30)
+
+library(ggmap)
+nycmap <- get_map(location = "Queens, New York", zoom=11)
+
+(nyc1 <- ggmap(nycmap) + scale_color_discrete(name = ">=15 Pedestrians Injured - 2013-2014")
+    + ggtitle(">=15 Pedestrians Injured - 2013-2014")
  + theme(plot.title=element_text(size=rel(2), face="bold"))
- + theme(axis.title=element_text(size=25), 
-         axis.text=element_text(size=16), 
-         #        legend.text=element_text(size=18),
-         legend.title=(element_text(size=18)))
- + guides(fill=FALSE)
- + labs(x="Contributing Factor", y="Observed Frequency", fill="CONTRIBUTING.FACTOR.VEHICLE.1"))
+ + theme(legend.text=element_text(size=0),
+         legend.title=(element_text(size=0)),
+        legend.position="none")
++ geom_point(data=high_injure_loc_ped, aes(LONGITUDE, LATITUDE, color = 'Pedestrians'), size=3))
 
 
 
+nycmap <- get_map(location = "Flatiron, New York", zoom=13)
+
+(nyc1 <- ggmap(nycmap) + scale_color_discrete(name = ">=15 Pedestrians Injured - 2013-2014")
+ + ggtitle(">=15 Pedestrians Injured - Mid, Lower Manhattan")
+ + theme(plot.title=element_text(size=rel(2), face="bold"))
+ + theme(legend.text=element_text(size=0),
+         legend.title=(element_text(size=0)),
+         legend.position="none")
+ + geom_point(data=high_injure_loc_ped, aes(LONGITUDE, LATITUDE, color = 'Pedestrians'), size=4))
+
+
+
+
+nycmap <- get_map(location = "Harlem, New York", zoom=14)
+
+(nyc1 <- ggmap(nycmap) + scale_color_discrete(name = ">=15 Pedestrians Injured - 2013-2014")
+ + ggtitle(">=15 Pedestrians Injured - Upper Manhattan & Bronx")
+ + theme(plot.title=element_text(size=rel(2), face="bold"))
+ + theme(legend.text=element_text(size=0),
+         legend.title=(element_text(size=0)),
+         legend.position="none")
+ + geom_point(data=high_injure_loc_ped, aes(LONGITUDE, LATITUDE, color = 'Pedestrians'), size=4))
 
 
 #BOROUGH
@@ -199,6 +297,41 @@ require(scales) # for removing scientific notation
          legend.position="bottom")
  #+ guides(fill=FALSE)
  + labs(x="Contributing Factor", y="Observed Frequency", fill="BOROUGH"))
+
+
+#PLOT VEHICLE TYPE AND INJURY
+inj_veh_typ <- ddply(filter(nycmvc_data, DATE>"2012-12-31" & DATE<"2015-01-01"),
+                     c("VEHICLE.TYPE.CODE.1"), 
+                     summarise,                 
+                     Pedestrians=sum(NUMBER.OF.PEDESTRIANS.INJURED),
+                     Cyclists=sum(NUMBER.OF.CYCLIST.INJURED),
+                     Motorists=sum(NUMBER.OF.MOTORIST.INJURED))
+
+tmp_melt <- melt(inj_veh_typ,
+                 id = c("VEHICLE.TYPE.CODE.1"),
+                 variable.name = "Injury_Category", value.name = "Frequency")
+
+tmp_melt_bar <- filter(tmp_melt, Injury_Category=="Pedestrians")
+tmp_melt_bar <- filter(tmp_melt, Injury_Category=="Cyclists")
+tmp_melt_bar <- filter(tmp_melt, Injury_Category=="Motorists")
+
+require(scales) # for removing scientific notation
+
+(ggplot(tmp_melt_bar, aes(x=reorder(VEHICLE.TYPE.CODE.1,Frequency), y=Frequency, fill=VEHICLE.TYPE.CODE.1)) 
+ + geom_bar(stat="identity")
+ + coord_flip()
+ #+ geom_text(aes(label=comma(Frequency)), vjust=-1, size=6, position=position_dodge(.9))
+ + scale_y_continuous(labels = comma)
+ + ggtitle("Injuries by Vehicle Type - 2013-2014 - Pedestrians")
+ # + ggtitle("Injuries by Vehicle Type - 2013-2014 - Cyclists")
+ # + ggtitle("Injuries by Vehicle Type - 2013-2014 - Motorists")
+ + theme(plot.title=element_text(size=rel(1), face="bold"))
+ + theme(axis.title=element_text(size=15), 
+         axis.text=element_text(size=10), 
+         #        legend.text=element_text(size=10),
+         legend.title=(element_text(size=10)))
+ + guides(fill=FALSE)
+ + labs(x="Injury Category", y="Observed Frequency", fill="Vehicle"))
 
 
 
